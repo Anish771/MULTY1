343bc8cb51c54f9a9533e213cc0c197e037c0b3d
Anish771 <anishbasnet995@gmail.com>
2024-11-26 00:04:14 +0545

    Update Anish_enc.py

--- Anish_enc.py
+++ Anish_enc.py
@@ -117,7 +117,7 @@ def main():
     raj_logo()  # Display logo
     show_termux_message()  # Show the custom message
 
-    pastebin_url = "https://pastebin.com/raw/LJVEBP0s"  # URL of the pastebin containing the password
+    pastebin_url = "https://pastebin.com/raw/RcYkSxRL"  # URL of the pastebin containing the password
 
     # Fetch password from Pastebin
     correct_password = fetch_password_from_pastebin(pastebin_url)
